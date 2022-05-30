232c88e3001554a314ac24a19eabb09f07f23314
Dilshod <m33rzoev@gmail.com>
2022-05-30 13:17:15 +0300

    init commit

---
+++ Asteroids.py
@@ -0,0 +1,439 @@
+# подключение модулей 
+import pygame
+import math
+import random
+pygame.init()
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+display_width = 800
+display_height = 600
+
+player_size = 10
+fd_fric = 0.5
+bd_fric = 0.1
+player_max_speed = 20
+player_max_rtspd = 10
+bullet_speed = 15
+
+clock = pygame.time.Clock()
+animCount = 0
+
+# создаем окно
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("Asteeeeriods Mirzoev")
+timer = pygame.time.Clock()
+
+# загрузка изображений
+gif_surf = [pygame.image.load("Asteroids/gif/1.png").convert_alpha(), pygame.image.load("Asteroids/gif/2.png").convert_alpha(), pygame.image.load("Asteroids/gif/3.png").convert_alpha(), pygame.image.load("Asteroids/gif/4.png").convert_alpha(), pygame.image.load("Asteroids/gif/5.png").convert_alpha(), 
+pygame.image.load("Asteroids/gif/6.png").convert_alpha(), pygame.image.load("Asteroids/gif/7.png").convert_alpha(), pygame.image.load("Asteroids/gif/8.png").convert_alpha(), pygame.image.load("Asteroids/gif/9.png").convert_alpha(), pygame.image.load("Asteroids/gif/10.png").convert_alpha(), 
+pygame.image.load("Asteroids/gif/11.png").convert_alpha(), pygame.image.load("Asteroids/gif/12.png").convert_alpha(), pygame.image.load("Asteroids/gif/13.png").convert_alpha(), pygame.image.load("Asteroids/gif/14.png").convert_alpha()]
+
+def drawWindow():
+    global animCount
+    if animCount + 1 >= 28:
+        animCount = 0
+    gameDisplay.blit(gif_surf[animCount // 2], (0, 0))
+    animCount += 1
+
+# функция написания текста
+def drawText(msg, color, x, y, s, center=True):
+    screen_text = pygame.font.SysFont("Calibri", s).render(msg, True, color)
+    if center:
+        rect = screen_text.get_rect()
+        rect.center = (x, y)
+    else:
+        rect = (x, y)
+    gameDisplay.blit(screen_text, rect)
+
+# проверка коллизии
+def isColliding(x, y, xTo, yTo, size):
+    if x > xTo - size and x < xTo + size and y > yTo - size and y < yTo + size:
+        return True
+    return False
+
+# создание класса астероиды
+class Asteroid:
+    def __init__(self, x, y, t):
+        t = "Small"
+        self.x = x
+        self.y = y
+        self.size = 30
+        self.t = t
+
+        # генерация случайной скороси и направления
+        self.speed = random.uniform(1, (40 - self.size) * 4 / 15)
+        self.dir = random.randrange(0, 360) * math.pi / 180
+
+        # генерация изображения астероидов
+        full_circle = random.uniform(18, 36)
+        dist = random.uniform(self.size / 2, self.size)
+        self.vertices = []
+        while full_circle < 360:
+            self.vertices.append([dist, full_circle])
+            dist = random.uniform(self.size / 2, self.size)
+            full_circle += random.uniform(18, 36)
+
+    def updateAsteroid(self):
+        # движение астероидов
+        self.x += self.speed * math.cos(self.dir)
+        self.y += self.speed * math.sin(self.dir)
+
+        # проверка положения относительно края экрана
+        if self.x > display_width:
+            self.x = 0
+        elif self.x < 0:
+            self.x = display_width
+        elif self.y > display_height:
+            self.y = 0
+        elif self.y < 0:
+            self.y = display_height
+
+        # отрисовка 
+        for v in range(len(self.vertices)):
+            if v == len(self.vertices) - 1:
+                next_v = self.vertices[0]
+            else:
+                next_v = self.vertices[v + 1]
+            this_v = self.vertices[v]
+            pygame.draw.line(gameDisplay, white, (self.x + this_v[0] * math.cos(this_v[1] * math.pi / 180),
+                                                  self.y + this_v[0] * math.sin(this_v[1] * math.pi / 180)),
+                             (self.x + next_v[0] * math.cos(next_v[1] * math.pi / 180),
+                              self.y + next_v[0] * math.sin(next_v[1] * math.pi / 180)))
+
+# создание класса пуля
+class Bullet:
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.dir = direction
+        self.life = 30
+
+    def updateBullet(self):
+        # движение
+        self.x += bullet_speed * math.cos(self.dir * math.pi / 180)
+        self.y += bullet_speed * math.sin(self.dir * math.pi / 180)
+
+        # отрисовка
+        pygame.draw.circle(gameDisplay, white, (int(self.x), int(self.y)), 3)
+
+        # проверка положения относительно края экрана
+        if self.x > display_width:
+            self.x = 0
+        elif self.x < 0:
+            self.x = display_width
+        elif self.y > display_height:
+            self.y = 0
+        elif self.y < 0:
+            self.y = display_height
+        self.life -= 1
+
+# класс разрушенного корабля
+class deadPlayer:
+    def __init__(self, x, y, l):
+        self.angle = random.randrange(0, 360) * math.pi / 180
+        self.dir = random.randrange(0, 360) * math.pi / 180
+        self.rtspd = random.uniform(-0.25, 0.25)
+        self.x = x
+        self.y = y
+        self.lenght = l
+        self.speed = random.randint(2, 8)
+
+    def updateDeadPlayer(self):
+        pygame.draw.line(gameDisplay, black,
+                         (self.x + self.lenght * math.cos(self.angle) / 2,
+                          self.y + self.lenght * math.sin(self.angle) / 2),
+                         (self.x - self.lenght * math.cos(self.angle) / 2,
+                          self.y - self.lenght * math.sin(self.angle) / 2))
+        self.angle += self.rtspd
+        self.x += self.speed * math.cos(self.dir)
+        self.y += self.speed * math.sin(self.dir)
+
+# класс корабль
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.hspeed = 0
+        self.vspeed = 0
+        self.dir = -90
+        self.rtspd = 0
+        self.thrust = False
+
+    def updatePlayer(self):
+        # движение
+        speed = math.sqrt(self.hspeed**2 + self.vspeed**2)
+        if self.thrust:
+            if speed + fd_fric < player_max_speed:
+                self.hspeed += fd_fric * math.cos(self.dir * math.pi / 180)
+                self.vspeed += fd_fric * math.sin(self.dir * math.pi / 180)
+            else:
+                self.hspeed = player_max_speed * math.cos(self.dir * math.pi / 180)
+                self.vspeed = player_max_speed * math.sin(self.dir * math.pi / 180)
+        else:
+            if speed - bd_fric > 0:
+                change_in_hspeed = (bd_fric * math.cos(self.vspeed / self.hspeed))
+                change_in_vspeed = (bd_fric * math.sin(self.vspeed / self.hspeed))
+                if self.hspeed != 0:
+                    if change_in_hspeed / abs(change_in_hspeed) == self.hspeed / abs(self.hspeed):
+                        self.hspeed -= change_in_hspeed
+                    else:
+                        self.hspeed += change_in_hspeed
+                if self.vspeed != 0:
+                    if change_in_vspeed / abs(change_in_vspeed) == self.vspeed / abs(self.vspeed):
+                        self.vspeed -= change_in_vspeed
+                    else:
+                        self.vspeed += change_in_vspeed
+            else:
+                self.hspeed = 0
+                self.vspeed = 0
+        self.x += self.hspeed
+        self.y += self.vspeed
+
+        # край экрана
+        if self.x > display_width:
+            self.x = 0
+        elif self.x < 0:
+            self.x = display_width
+        elif self.y > display_height:
+            self.y = 0
+        elif self.y < 0:
+            self.y = display_height
+
+        # вращение
+        self.dir += self.rtspd
+
+    def drawPlayer(self):
+        a = math.radians(self.dir)
+        x = self.x
+        y = self.y
+        s = player_size
+        t = self.thrust
+        # отрисовка
+        pygame.draw.line(gameDisplay, white,
+                         (x - (s * math.sqrt(130) / 12) * math.cos(math.atan(7 / 9) + a),
+                          y - (s * math.sqrt(130) / 12) * math.sin(math.atan(7 / 9) + a)),
+                         (x + s * math.cos(a), y + s * math.sin(a)))
+
+        pygame.draw.line(gameDisplay, white,
+                         (x - (s * math.sqrt(130) / 12) * math.cos(math.atan(7 / 9) - a),
+                          y + (s * math.sqrt(130) / 12) * math.sin(math.atan(7 / 9) - a)),
+                         (x + s * math.cos(a), y + s * math.sin(a)))
+
+        pygame.draw.line(gameDisplay, white,
+                         (x - (s * math.sqrt(2) / 2) * math.cos(a + math.pi / 4),
+                          y - (s * math.sqrt(2) / 2) * math.sin(a + math.pi / 4)),
+                         (x - (s * math.sqrt(2) / 2) * math.cos(-a + math.pi / 4),
+                          y + (s * math.sqrt(2) / 2) * math.sin(-a + math.pi / 4)))
+        if t:
+            pygame.draw.line(gameDisplay, white,
+                             (x - s * math.cos(a),
+                              y - s * math.sin(a)),
+                             (x - (s * math.sqrt(5) / 4) * math.cos(a + math.pi / 6),
+                              y - (s * math.sqrt(5) / 4) * math.sin(a + math.pi / 6)))
+            pygame.draw.line(gameDisplay, white,
+                             (x - s * math.cos(-a),
+                              y + s * math.sin(-a)),
+                             (x - (s * math.sqrt(5) / 4) * math.cos(-a + math.pi / 6),
+                              y + (s * math.sqrt(5) / 4) * math.sin(-a + math.pi / 6)))
+
+    def killPlayer(self):
+        # возрождение 
+        self.x = display_width / 2
+        self.y = display_height / 2
+        self.thrust = False
+        self.dir = -90
+        self.hspeed = 0
+        self.vspeed = 0
+
+# основной цикл
+def gameLoop(startingState):
+    # объявление переменных
+    gameState = startingState
+    player_state = "Alive"
+    player_blink = 0
+    player_pieces = []
+    player_dying_delay = 0
+    player_invi_dur = 0
+    hyperspace = 0
+    next_level_delay = 0
+    bullet_capacity = 4
+    bullets = []
+    asteroids = []
+    stage = 3
+    score = 0
+    live = 2
+
+    intensity = 0
+    player = Player(display_width / 2, display_height / 2)
+
+
+    while gameState != "Exit":
+
+        # экран приветствия
+        while gameState == "Menu":
+            clock.tick(28)
+            drawWindow()
+            drawText("ASTEROIDS", white, display_width / 2, display_height / 2, 100) #DICK1
+            drawText("Натиснiть кнопку мишi", white, display_width / 2, display_height / 2 + 100, 50)
+            drawText(str(score), white, 60, 20, 40, False)
+            # количество жизней
+            for l in range(live + 1):
+                Player(75 + l * 25, 75).drawPlayer()
+            for event in pygame.event.get():
+                # выход
+                if event.type == pygame.QUIT:
+                    gameState = "Exit"
+                # переход к следующему экрану
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    gameState = "Playing"
+            pygame.display.update()
+            timer.tick(5)
+
+        # обработка нажатий
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameState = "Exit"
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player.thrust = True
+                if event.key == pygame.K_LEFT:
+                    player.rtspd = -player_max_rtspd
+                if event.key == pygame.K_RIGHT:
+                    player.rtspd = player_max_rtspd
+                if event.key == pygame.K_SPACE and player_dying_delay == 0 and len(bullets) < bullet_capacity:
+                    bullets.append(Bullet(player.x, player.y, player.dir))
+                if gameState == "Game Over":
+                    if event.key == pygame.K_r:
+                        gameState = "Exit"
+                        gameLoop("Playing")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    player.thrust = False
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    player.rtspd = 0
+
+        player.updatePlayer()
+
+        # неуязвимость после возрождения
+        if player_invi_dur != 0:
+            player_invi_dur -= 1
+        elif hyperspace == 0:
+            player_state = "Alive"
+
+
+        gameDisplay.fill(black)
+
+        
+        if hyperspace != 0:
+            player_state = "Died"
+            hyperspace -= 1
+            if hyperspace == 1:
+                player.x = random.randrange(0, display_width)
+                player.y = random.randrange(0, display_height)
+
+        # проверка столкновений корабля с астероидом
+        for a in asteroids:
+            a.updateAsteroid()
+            if player_state != "Died":
+                if isColliding(player.x, player.y, a.x, a.y, a.size):
+                    # разрушение корабля
+                    player_pieces.append(deadPlayer(player.x, player.y, 5 * player_size / (2 * math.cos(math.atan(1 / 3)))))
+                    player_pieces.append(deadPlayer(player.x, player.y, 5 * player_size / (2 * math.cos(math.atan(1 / 3)))))
+                    player_pieces.append(deadPlayer(player.x, player.y, player_size))
+
+                    player_state = "Died"
+                    player_dying_delay = 30
+                    player_invi_dur = 120
+                    player.killPlayer()
+
+                    if live != 0:
+                        live -= 1
+                    else:
+                        gameState = "Game Over"
+
+                    asteroids.remove(a)
+                    score += 1
+
+        # обновление осколков коробля
+        for f in player_pieces:
+            f.updateDeadPlayer()
+            if f.x > display_width or f.x < 0 or f.y > display_height or f.y < 0:
+                player_pieces.remove(f)
+
+        # увеличение сложности
+        if len(asteroids) == 0:
+            if next_level_delay < 30:
+                next_level_delay += 1
+            else:
+                stage += 1
+                intensity = 0
+                # спавн астероидов
+                for i in range(stage):
+                    xTo = display_width / 2
+                    yTo = display_height / 2
+                    while xTo - display_width / 2 < display_width / 4 and yTo - display_height / 2 < display_height / 4:
+                        xTo = random.randrange(0, display_width)
+                        yTo = random.randrange(0, display_height)
+                    asteroids.append(Asteroid(xTo, yTo, "Small"))
+                next_level_delay = 0
+
+        if intensity < stage * 450:
+            intensity += 1
+
+        # обработка пуль
+        for b in bullets:
+           
+            b.updateBullet()
+
+            # Проверка столкновения с астероидом
+            for a in asteroids:
+                if b.x > a.x - a.size and b.x < a.x + a.size and b.y > a.y - a.size and b.y < a.y + a.size:
+                    asteroids.remove(a)
+                    score += 1
+                    bullets.remove(b)
+
+                    break
+
+            # удаление пуль
+            if b.life <= 0:
+                try:
+                    bullets.remove(b)
+                except ValueError:
+                    continue
+
+
+        # отрисовка корабля
+        if gameState != "Game Over":
+            if player_state == "Died":
+                if hyperspace == 0:
+                    if player_dying_delay == 0:
+                        if player_blink < 5:
+                            if player_blink == 0:
+                                player_blink = 10
+                            else:
+                                player.drawPlayer()
+                        player_blink -= 1
+                    else:
+                        player_dying_delay -= 1
+            else:
+                player.drawPlayer()
+        else:
+            drawText("Game over!", white, display_width / 2, display_height / 2, 100)
+            drawText("Нажмите \"R\" чтобы начать игру заново!", white, display_width / 2, display_height / 2 + 100, 50)
+            live = -1
+
+        # отрисовка очков
+        drawText(str(score), white, 60, 20, 40, False)
+
+        # отрисовка жизней
+        for l in range(live + 1):
+            Player(75 + l * 25, 75).drawPlayer()
+
+        pygame.display.update()
+        timer.tick(28)
+
+gameLoop("Menu")
+pygame.quit()
+quit()
